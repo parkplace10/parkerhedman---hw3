b7c350fe4be618b0a8876efdf41e73ac4b08f44b
Parker Hedman <parkerbhedman@gmail.com>
2018-10-13 23:07:13 -0500

    Update pyBank/pybank_main.py

--- pyBank/pybank_main.py
+++ pyBank/pybank_main.py
@@ -13,6 +13,13 @@ with open(csv_path,newline = '') as csvfile:
     for row in csvreader:
         months.append(row[0])
         profit_loss.append(row[1])
+       
+
+#Financial Analysis
+print("""
+Financial Analysis
+------------------------------------""")
+
 
 #Total Months
 print ("Total Months: " + str(len(months)))
@@ -20,8 +27,44 @@ print ("Total Months: " + str(len(months)))
 #Total
 profit_loss = [int(i) for i in profit_loss]
 total = sum(profit_loss)
-print("Total: " + str(total))
+total_formatted = '${:,.2f}'.format(total)
+print("Total: " + str(total_formatted))
 
 #Average Change
+profit_change = 0
+monthly_change = []
+num_months = range(len(months)-1)
+for month in num_months:
+    monthly_change.append(int(profit_loss[month+1]) - int(profit_loss[month]))
 
+count_months = len(months)-1
+average_change = sum(monthly_change) / count_months
+average_change_formatted = '${:,.2f}'.format(average_change)
+print("Average change: " + str(average_change_formatted))
+
+#Greatest Increase
+greatest_increase = 0
+greatest_increase_month_index = 0
+for month in num_months:
+    if monthly_change[month]>greatest_increase:
+        greatest_increase = monthly_change[month]
+        greatest_increase_month_index = month + 1
+
+greatest_increase_month = months[greatest_increase_month_index]
+greatest_increase_formatted = '${:,.2f}'.format(greatest_increase)
+print(f"Greatest increase: {greatest_increase_formatted} ({greatest_increase_month})")
+
+#Greatest Decrease
+greatest_decrease = 0
+greatest_decrease_month_index = 0
+for month in num_months:
+    if monthly_change[month]<greatest_decrease:
+        greatest_decrease = monthly_change[month]
+        greatest_decrease_month_index = month + 1
+
+greatest_decrease_month = months[greatest_decrease_month_index]
+greatest_decrease_formatted = '${:,.2f}'.format(greatest_decrease)
+print(f"Greatest increase: {greatest_decrease_formatted} ({greatest_decrease_month})")
+
+print("------------------------------------")   
 
